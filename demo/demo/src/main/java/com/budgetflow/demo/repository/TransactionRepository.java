59267a6db28239caf2e6b733842aed1fb943e08c
cesam <matthew-frederick.cesa@etu.u-paris.fr>
2026-06-04 13:30:11 +0200

    first commit

---
+++ demo/demo/src/main/java/com/budgetflow/demo/repository/TransactionRepository.java
@@ -0,0 +1,5 @@
+package com.budgetflow.demo.repository;
+
+public interface TransactionRepository {
+    
+}
---
+++ demo/demo/src/test/java/com/budgetflow/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetflow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
